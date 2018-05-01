35775a35b3b758e15a2477cc0ded5cb9b84ddad0
Rubbbery89 <julitom25@gmail.com>
2018-05-01 17:39:51 -0500

    clean datacontext

--- HealthCenter/HealthCenter.Backend/Models/LocalDataContext.cs
+++ HealthCenter/HealthCenter.Backend/Models/LocalDataContext.cs
@@ -3,11 +3,7 @@
     using Domain;
 
     public class LocalDataContext : DataContext
-    {
-        public System.Data.Entity.DbSet<HealthCenter.Domain.User> Users { get; set; }
-
-        public System.Data.Entity.DbSet<HealthCenter.Domain.UserType> UserTypes { get; set; }
-
+    {       
         public System.Data.Entity.DbSet<HealthCenter.Backend.Models.Agenda> Agenda { get; set; }
 
         public System.Data.Entity.DbSet<HealthCenter.Backend.Models.WorkDaySchedule> WorkDays { get; set; }
